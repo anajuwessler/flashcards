f5ee4b0e1b49fae784febf06fe4251a57719c5c0
julia <ana.wessler.nascimento@escola.pr.gov.br>
2025-07-29 17:08:38 -0300

    app.js

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,24 @@
-function criaCartao(categoria, pergunta, resposta){
-    'PROGRAMAÇÃO'
-    'O que é JavaScript?'
-    'O JavaScript é uma linguagem de programação.'
-}
+
+criaCartao(
+    'PROGRAMAÇÃO',
+   'O que é JavaScript?',
+   'O JavaScript é uma linguagem de programação.'
+)
 
 criaCartao(
     'HISTÓRIA',
-    'Por que a Revolução Francesa foi um marco importante na história mundial?',
-    'O JavaScript é uma linguagem de estilização.',
+   'Por que a Revolução Francesa foi um marco importante na história mundial?',
+   'O JavaScript é uma linguagem de estilização.'
+
 )
 
-criaCartao(
-    'GEOGRAFIA'
-    'Por que a China tem papel importante na guerra entre Rússia e Ucrânia?'
-    'Porque ela acabou com a monarquia absolutista na França, espalhou ideias de liberdade, igualdade e fraternidade, e inspirou movimentos democráticos em várias partes do mundo.'
+criaCartao(    
+'GEOGRAFIA',
+   'Por que a China tem papel importante na guerra entre Rússia e Ucrânia?',
+   'Porque ela acabou com a monarquia absolutista na França, espalhou ideias de liberdade, igualdade e fraternidade, e inspirou movimentos democráticos em várias partes do mundo.'
+
 )
+
+function criaCartao(Categoria, pergunta, resposta){
+    console.log(Categoria, pergunta, resposta);
+}
